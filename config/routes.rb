9e13bc64771bd7ea25f46e173c7a4e007b52d3f2
Your Name <you@example.com>
2023-02-09 22:02:09 +0300

    Add cart model, add current_cart module

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   resources :products
   root 'store#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_08_222627) do
+ActiveRecord::Schema.define(version: 2023_02_09_184933) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
